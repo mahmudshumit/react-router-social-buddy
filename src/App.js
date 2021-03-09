f05616e0184012cdd22c092c83e914c62af2fa7b
mahmudshumit <rahmanshumit@gmail.com>
2021-03-09 19:11:08 +0600

    Finished' git commit -m Finished git commit -mFinished git commit -m""Finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Components/Home';
+import About from './Components/About/About';
+import PostDetail from './Components/PostDetail/PostDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/home">Home2</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+     
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/post/:id">
+          <PostDetail></PostDetail>
+
+        </Route>
+        <Route path="/about">
+          <About></About>
+          
+        </Route>
+        <Route path="/dashboard">
+          
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
